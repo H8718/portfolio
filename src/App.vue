6a6296f9e86c4fa5074357df7e26bdc870999ce2
Markus Moilanen <moilamar@protonmail.com>
2017-12-16 18:44:02 +0200

    Setting up

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,30 @@
 </template>
 
 <script>
-import Banner from './components/Banner';
+import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import About from "./components/About";
 
 export default {
-  name: 'app',
-  components: {
-    Banner
-  }
-}
+    name: "app",
+    components: {
+        Banner
+    }
+};
 </script>
 
 <style lang="scss">
-    #site {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        color: #2c3e50;
-    }
+body {
+    background-color: #f9f9f9;
+    margin: 0;
+    padding: 0;
+}
+#site {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    // font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+}
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- <Navbar inverse collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a id="brand" href="#banner"  className="smoothScroll">
+            <img src="../assets/m.jpg" alt="m"/>
+            <div id="markus">Markus</div> <div id="moilanen">Moilanen</div>
+        </a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href="#about"><div>About Me</div></NavItem>
+        <NavItem eventKey={2} href="#skills"><div>Skills</div></NavItem>
+        <NavItem eventKey={3} href="#projects"><div>Projects</div></NavItem>
+        <NavItem eventKey={4} href="#contact"><div>Contact</div></NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar> -->
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style lang="scss">
+
+</style>
